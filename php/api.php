6ce443f7f9fd6c25f8fd07589ce3fae7bae73a9e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-12 23:00:00 +0100

    fix cors error

--- php/api.php
+++ php/api.php
@@ -1,25 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Headers: X-Request-Width");
+header("Access-Control-Allow-Headers: X-Requested-Width");
 
 header("Content-Type: application/json");
 
 $value = $_GET["fare"];
 
-
-// $toDo = [
-//     [
-//       "id" => 1,
-//       "task" => "Fare la spesa",
-//       "completed" => false,
-//       "fare" => $value
-//     ],
-//     [
-//       "id"=> 2,
-//       "task" => "Lavare i piatti",
-//       "completed" => true
-//     ]
-// ];
 $jsonTodoList = file_get_contents("todo.json", true);
 $todoList = json_decode($jsonTodoList);
 
@@ -27,7 +13,7 @@ $toDo[]= [
       "task" => "Fare la spesa",
       "completed" => false,
       "fare" => $value
-],
+];
 
 $jsonTodoList = json_encode($toDo);
 file_put_contents("todo.json", $jsonTodoList);
