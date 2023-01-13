ac4d28cc782fd7322c457e2fae7e3d3ba645524c
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-13 10:55:16 +0100

    remake

--- frontend/src/components/Todo.vue
+++ frontend/src/components/Todo.vue
@@ -1,37 +1,14 @@
-<script>
-import axios from 'axios';
+<script >
 
-const API_URL = "http://localhost/api.php";
 
-export default {
-    data(){
-        return{
-            newTask :""
-        }
-    },
-    methods:{
-    addNew(e){
-        e.preventDefault();
-        axios.get(API_URL, { params:  {     "id" : 100,
-                                            "task" : this.newTask,
-                                            "completed" : false,
-                                            },})
-        .then(res => {
-        const data = res.data;
-        console.log(data);
-
-    })
-    },
- }    
-}
 </script>
 
 <template>
-<form action="" @submit="addNew" value="toDo">
-    <label for="">Inserisci nuovo</label>
-    <input type="text" name="todo" v-model="newTask" >
-    <input type="submit">
-</form>
+
+  <div class="card">
+    
+  </div>
+
 </template>
 
 <style scoped>
--- php/api.php
+++ php/api.php
@@ -4,21 +4,5 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 header("Content-Type: application/json");
 
-$value = $_GET["task"];
-
-$jsonTodoList = file_get_contents("./todo.json", true);
-$todoList = json_decode($jsonTodoList);
-
-$toDo[]= [
-      "id" => 100,
-      "task" => $value,
-      "completed" => false,
- 
-];
-
-
-var_dump($toDo);
-file_put_contents("todo.json", json_encode($toDo));
-
 
 ?>
