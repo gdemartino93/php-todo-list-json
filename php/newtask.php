85cb65743f7dc0214db1dd1fe9196d8408a394ba
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-13 12:21:33 +0100

    check on empty task

--- php/newtask.php
+++ php/newtask.php
@@ -6,15 +6,21 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 header("Content-Type: application/json");
 
 $newTask = $_GET["newTask"];
+if ( $newTask !== ""){
 
-$jsonTodoList = file_get_contents("todo.json", true);
-$todoList = json_decode($jsonTodoList);
+    $jsonTodoList = file_get_contents("todo.json", true);
+    $todoList = json_decode($jsonTodoList);
+    
+    
+    $todoList[] = [
+        "text" => $newTask,
+        "completed" => false
+    ];
+    
+    
+    
+    
+    $jsonTodoList = json_encode($todoList);
+    file_put_contents("todo.json" , $jsonTodoList);
+}
 
-
-$todoList[] = [
-    "text" => $newTask,
-    "completed" => false
-];
-
-$jsonTodoList = json_encode($todoList);
-file_put_contents("todo.json" , $jsonTodoList);
