276aac1b1ae49089923967783743ffb141ebdbb3
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-17 01:16:34 +0100

    header component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Header from './components/AppHeader.vue'
 import Todo from './components/Todo.vue'
 </script>
 
 <template>
+    <header>
+        <Header />
+    </header>
     <div class="container d-flex justify-content-center align-items-center">
         <Todo />
     </div>
@@ -10,5 +14,9 @@ import Todo from './components/Todo.vue'
 
 <style scoped>
 
+header{
+    background-color:beige;
+    width: 100vw;
+}
 
 </style>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script >
+
+export default {
+
+
+}
+</script>
+
+<template>
+  
+  <h1>To-Do App</h1>
+
+</template>
+
+<style scoped>
+
+</style>
